14c6be2434eee82fea1a0fd4d8f68244e7b38af1
Hakkology <yildizhakan88@gmail.com>
2024-05-25 08:56:32 +0300

    enemy skeleton battle state move

--- Assets/Scripts/Enemy/EnemySkeletonBattleState.cs
+++ Assets/Scripts/Enemy/EnemySkeletonBattleState.cs
@@ -1,18 +1,34 @@
-public class EnemySkeletonBattleState : EnemySkeletonGroundedState
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemySkeletonBattleState : EnemyState
 {
-    public EnemySkeletonBattleState(Enemy _enemy, EnemyStateMachine _stateMachine, string _animBoolName, EnemySkeleton _enemySkeleton) : base(_enemy, _stateMachine, _animBoolName, _enemySkeleton)
+    private Transform player;
+    private EnemySkeleton enemySkeleton;
+    private float moveDirection;
+    public EnemySkeletonBattleState(Enemy _enemy, EnemyStateMachine _stateMachine, string _animBoolName, EnemySkeleton _enemySkeleton) : base(_enemy, _stateMachine, _animBoolName)
     {
-        
+        this.enemySkeleton = _enemySkeleton;
     }
 
     public override void Enter()
     {
         base.Enter();
+
+        player = GameObject.Find("Player").transform;
     }
 
     public override void Update()
     {
         base.Update();
+
+        if (player.position.x > enemySkeleton.transform.position.x)
+            moveDirection = 1;
+        else
+            moveDirection = -1;
+        
+        enemy.SetVelocity(enemySkeleton.moveSpeed * moveDirection, enemySkeleton.rb.velocity.y);
+        
     }
 
     public override void Exit()
